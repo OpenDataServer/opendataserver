a0ee907d342e2e609b8d34ddfdc753fbaa2efffd
Leo Maroni <me@leomaroni.de>
2019-12-12 18:15:20 +0100

    EditorConfig added

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
 from .models import User
 
 
