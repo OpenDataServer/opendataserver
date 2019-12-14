28261ba309e292b7d8631b51620c8323cb323a53
Leo Maroni <me@leomaroni.de>
2019-12-14 16:57:06 +0100

    Added tests for user model and added errors in user model

--- accounts/managers.py
+++ accounts/managers.py
@@ -2,23 +2,35 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email: str, password: str = None, **kwargs):
+    def create_user(self, email: str, first_name: str, password: str, last_name: str = ""):
         if not email:
-            raise ValueError(_('The Email must be set'))
+            raise ValueError('The Email must be set')
+        if not first_name:
+            raise ValueError('The first name must be set')
+        if not password:
+            raise ValueError('The password must be set')
         user = self.model(
             email=email,
-            **kwargs
+            first_name=first_name,
+            last_name=last_name
         )
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email: str, first_name: str, password: str = None):
+    def create_superuser(self, email: str, first_name: str, password: str, last_name: str = ""):
+        if not email:
+            raise ValueError('The Email must be set')
+        if not first_name:
+            raise ValueError('The first name must be set')
+        if not password:
+            raise ValueError('The password must be set')
         user = self.model(
             email=email,
             first_name=first_name,
+            last_name=last_name
         )
-        user.set_password(password)
         user.is_staff = True
+        user.set_password(password)
         user.save()
         return user
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,1 +1,73 @@
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+        user_model = get_user_model()
+        user = user_model.objects.create_user(
+            email='normal@example.org',
+            first_name="Normal",
+            last_name="User",
+            password='foobar'
+        )
+        self.assertEqual(user.email, 'normal@example.org')
+        self.assertEqual(user.first_name, 'Normal')
+        self.assertEqual(user.last_name, 'User')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        with self.assertRaises(TypeError):
+            user_model.objects.create_user()
+        with self.assertRaises(ValueError):
+            user_model.objects.create_user(
+                email='',
+                first_name='Normal',
+                password="foobar"
+            )
+        with self.assertRaises(ValueError):
+            user_model.objects.create_user(
+                email='normal2@exmaple.org',
+                first_name='',
+                password='foobar'
+            )
+        with self.assertRaises(ValueError):
+            user_model.objects.create_user(
+                email='normal3@exmaple.org',
+                first_name='Normal',
+                password=''
+            )
+
+    def test_create_superuser(self):
+        user_model = get_user_model()
+        admin_user = user_model.objects.create_superuser(
+            email='super@example.org',
+            first_name="Super",
+            last_name="user_model",
+            password='foobar'
+        )
+        self.assertEqual(admin_user.email, 'super@example.org')
+        self.assertEqual(admin_user.first_name, 'Super')
+        self.assertEqual(admin_user.last_name, 'user_model')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        with self.assertRaises(TypeError):
+            user_model.objects.create_superuser()
+        with self.assertRaises(ValueError):
+            user_model.objects.create_superuser(
+                email='',
+                first_name='Super',
+                password="foobar"
+            )
+        with self.assertRaises(ValueError):
+            user_model.objects.create_superuser(
+                email='super2@exmaple.org',
+                first_name='',
+                password='foobar'
+            )
+        with self.assertRaises(ValueError):
+            user_model.objects.create_superuser(
+                email='super3@exmaple.org',
+                first_name='Super',
+                password=''
+            )
