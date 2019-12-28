60bd855abed9ed8bce9b7241bf7e8c89df5f6ed1
Leo Maroni <me@leomaroni.de>
2019-12-28 08:10:08 +0100

    Added Project member settings

--- projects/urls.py
+++ projects/urls.py
@@ -6,5 +6,11 @@ app_name = 'projects'
 
 urlpatterns = [
     path('', project_list.project_list, name='list'),
-    path('details/<int:project_id>/settings/general', project_settings.general, name="project_settings_general")
+    path('details/<int:project_id>/settings/general', project_settings.general, name="project_settings_general"),
+    path('details/<int:project_id>/settings/members', project_settings.members, name="project_settings_members"),
+    path('details/<int:project_id>/settings/members/<int:member_id>', project_settings.members_edit,
+         name="project_settings_members_edit"),
+    path('details/<int:project_id>/settings/members/new', project_settings.members_new,
+         name="project_settings_members_new")
+
 ]
--- projects/views/project_settings.py
+++ projects/views/project_settings.py
@@ -1,14 +1,8 @@
-from django import forms
 from django.shortcuts import render, get_object_or_404, redirect
 
-from projects.models import Project
-
-
-class GeneralSettingsForm(forms.ModelForm):
-
-    class Meta:
-        model = Project
-        fields = ('name',)
+from accounts.models import User
+from projects.forms.project_settings import MemberNewSettingsForm, GeneralSettingsForm, MemberEditSettingsForm
+from projects.models import Project, ProjectMember
 
 
 def general(request, project_id):
@@ -29,4 +23,79 @@ def general(request, project_id):
             request=request,
             template_name='details/settings/general.html',
             context={'form': form}
+        )
+
+
+def members(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    project_members = ProjectMember.objects.filter(
+        project=project
+    )
+    return render(
+        request=request,
+        template_name='details/settings/members.html',
+        context={'members': project_members}
+    )
+
+
+def members_edit(request, project_id, member_id):
+    project_member = get_object_or_404(
+        ProjectMember,
+        id=member_id,
+        project_id=project_id
+    )
+    if request.method == 'POST':
+        form = MemberEditSettingsForm(request.POST)
+        if form.is_valid():
+            project_member.role = form.cleaned_data['role']
+            project_member.save()
+            return redirect('projects:project_settings_members', project_id=project_id)
+        return render(
+            request=request,
+            template_name='details/settings/members_new.html',
+            context={
+                'form': form
+            }
+        )
+    else:
+        form = MemberEditSettingsForm(
+            initial={
+                "role": project_member.role
+            })
+        return render(
+            request=request,
+            template_name='details/settings/members_edit.html',
+            context={
+                'form': form,
+                'project_member': project_member
+            }
+        )
+
+
+def members_new(request, project_id):
+    if request.method == 'POST':
+        form = MemberNewSettingsForm(request.POST)
+        if form.is_valid():
+            user = User.objects.get(email=form.cleaned_data['email'])
+            project = Project.objects.get(id=project_id)
+            if ProjectMember.objects.filter(user=user, project=project).count() == 0:
+                ProjectMember.objects.create(user=user, role=form.cleaned_data['role'], project=project)
+                return redirect('projects:project_settings_members', project_id=project_id)
+            else:
+                form.add_error("email", "The user is already member in the project")
+        return render(
+            request=request,
+            template_name='details/settings/members_new.html',
+            context={
+                'form': form
+            }
+        )
+    else:
+        form = MemberNewSettingsForm()
+        return render(
+            request=request,
+            template_name='details/settings/members_new.html',
+            context={
+                'form': form
+            }
         )
